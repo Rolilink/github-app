72c91556632803e00bcc16638334a6e75572b9fb
Rolando Perez <me@rolilink.com>
2017-12-16 16:43:00 -0500

    setup debugging

--- Store.js
+++ Store.js
@@ -4,7 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 export default ({ reducers = {}, middlewares = [] }) => (
   createStore(
     combineReducers(reducers),
-    //composeWithDevTools(applyMiddleware(...middlewares)), //use this to debug
-    applyMiddleware(...middlewares),
+    composeWithDevTools(applyMiddleware(...middlewares)), //use this to debug
+    //applyMiddleware(...middlewares)
   )
 );
