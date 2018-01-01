2eda02d92d12b2bd3b3048d296051a655719079e
Rolando Perez <me@rolilink.com>
2017-12-31 19:30:50 -0500

    updated instructions

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NativeModules } from 'react-native';
 import { Provider } from 'react-redux';
 import PromiseMiddleware from 'redux-promise-middleware';
-import Expo from 'expo';
+import Expo from 'expo'; // comment this line when running without expo
 import Router from './Router';
 import routes from './routes';
 import reducers from './reducers';
@@ -13,13 +13,14 @@ NativeModules.DevSettings.setIsDebuggingRemotely(true); // uncomment to debug
 const store = createStore({ reducers, middlewares: [new PromiseMiddleware()] });
 
 export default class App extends React.Component {
+// /* uncomment this line when running without expo
   async componentWillMount() {
     await Expo.Font.loadAsync({
       Roboto: require('native-base/Fonts/Roboto.ttf'),
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
     });
   }
-
+// */ uncomment this line when running without expo
   render() {
     return (
       <Provider store={store}>
